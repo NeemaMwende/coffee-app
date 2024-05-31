406a3f49a0df03c3d8f5513324e2eb70eb37f8cb
NeemaMwende <gracepete12@gmail.com>
2024-05-31 21:20:53 +0300

    updates

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Home.css";
+
+const Home = () => {
+  return (
+        <section class="home" id="home">
+            <div class="content">
+                <h3>Fresh coffee in the morning</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                    Nulla iure ad quos vero nam culpa, minima ipsum nemo, 
+                    natus fugit, quas a? Architecto ut omnis accusamus libero neque asperiores impedit.</p>
+                    <div class="button">Get yours now</div>
+            </div>
+        </section>
+  )
+}
+
+export default Home;
